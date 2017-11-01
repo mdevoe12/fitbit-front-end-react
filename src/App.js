b7901673f7040c3bce9ab291d1151a92cfdbbebd
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-01 16:13:45 -0600

    login or logout button renders based on token presence

--- src/App.js
+++ src/App.js
@@ -3,28 +3,55 @@ import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import FitbitApiClient from 'fitbit-node';
-// let token = window.location.search.substring(1);
-// let qs = parse_query_string(token)
 let current_url = window.location.href
-let raw_token = current_url.split('token=')[1]
-let token = raw_token.replace(/#/, "").replace(/_/g, "").replace(/=/g, "")
+let token = ""
 
+
+
+
+function setToken() {
+  if (current_url.includes("token")) {
+    let raw_token = current_url.split('token=')[1]
+    token = raw_token.replace(/#/, "").replace(/_/g, "").replace(/=/g, "")
+  } else {
+    return {isLoggedIn: false}
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = setToken();
+  }
+
+  handleLogoutClick = () => {
+    axios.delete('')
+  }
+
   handleOnClick = () => {
     axios.get('http://localhost:3000/api/v1/keys')
     .then((response) => {
-      // let client = new FitbitApiClient(response.data["id"], response.data["secret"])
-      // let scope = 'activity heartrate location nutrition profile settings sleep social weight'
-      // let callback = 'http://localhost:3000/auth/fitbit/callback'
+      let client = new FitbitApiClient(response.data["id"], response.data["secret"])
+      let scope = 'activity heartrate location nutrition profile settings sleep social weight'
+      let callback = 'http://localhost:3000/auth/fitbit/callback'
       console.log(token)
-      // window.location.href = client.getAuthorizeUrl(scope, callback)
+      window.location.href = client.getAuthorizeUrl(scope, callback)
     })
   }
 
 
   render() {
-    // debugger
+    console.log(token)
+    let button = null;
+    if (token == "") {
+      button = <button text="Login with FitBit" onClick={this.handleOnClick} type="button">
+      Login with FitBit
+      </button>
+    } else {
+      button = <button text="Login with FitBit" onClick={this.handleLogoutClick} type="button">
+      Logout
+      </button>
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -37,9 +64,7 @@ class App extends Component {
         <p className="App-intro">
           Click the button below to begin
         </p>
-        <button text="Login with FitBit" onClick={this.handleOnClick} type="button">
-        Login with FitBit
-        </button>
+        {button}
       </div>
     );
   }
