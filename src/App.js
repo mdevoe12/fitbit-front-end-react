5f17ea5cea9dec41a613de486106b21a849b750d
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-01 13:41:13 -0600

    final setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import FitbitApiClient from 'fitbit-node';
+let test = "test";
+
 
 class App extends Component {
   handleOnClick = () => {
@@ -11,10 +13,14 @@ class App extends Component {
       let client = new FitbitApiClient(response.data["id"], response.data["secret"])
       let scope = 'activity heartrate location nutrition profile settings sleep social weight'
       let callback = 'http://localhost:3000/auth/fitbit/callback'
+      console.log(test)
       window.location.href = client.getAuthorizeUrl(scope, callback)
     })
   }
+
+
   render() {
+    // debugger
     return (
       <div className="App">
         <header className="App-header">
