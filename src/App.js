970a788a02c4b6d6e2e6b280d7cb07a4f86d6ac9
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-04 16:04:06 -0600

    working on rendering data on React side

--- src/App.js
+++ src/App.js
@@ -9,19 +9,20 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'r
 let api = "http://localhost:3000"
 let current_url = window.location.href
 let token = ""
+let data = null
 
-let data = [      {name: '10/01', sleep: 400, weight: 85, amt: 240},
-                  {name: '10/02', sleep: 300, weight: 90, amt: 221},
-                  {name: '10/03', sleep: 200, weight: 89, amt: 220},
-                  {name: '10/04', sleep: 180, weight: 88, amt: 200},
-                  {name: '10/05', sleep: 0,   weight: 87, amt: 218},
-                  {name: '10/06', sleep: 100, weight: 86, amt: 250},
-                  {name: '10/07', sleep: 300, weight: 87, amt: 221},
-                  {name: '10/08', sleep: 200, weight: 89, amt: 290},
-                  {name: '10/09', sleep: 278, weight: 91, amt: 200},
-                  {name: '10/10', sleep: 189, weight: 92, amt: 281},
-                  {name: '10/11', sleep: 239, weight: 91, amt: 250},
-                  {name: '10/12', sleep: 349, weight: 89, amt: 210},]
+// let data = [      {name: '10/01', sleep: 400, weight: 85, amt: 240},
+//                   {name: '10/02', sleep: 300, weight: 90, amt: 221},
+//                   {name: '10/03', sleep: 200, weight: 89, amt: 220},
+//                   {name: '10/04', sleep: 180, weight: 88, amt: 200},
+//                   {name: '10/05', sleep: 0,   weight: 87, amt: 218},
+//                   {name: '10/06', sleep: 100, weight: 86, amt: 250},
+//                   {name: '10/07', sleep: 300, weight: 87, amt: 221},
+//                   {name: '10/08', sleep: 200, weight: 89, amt: 290},
+//                   {name: '10/09', sleep: 278, weight: 91, amt: 200},
+//                   {name: '10/10', sleep: 189, weight: 92, amt: 281},
+//                   {name: '10/11', sleep: 239, weight: 91, amt: 250},
+//                   {name: '10/12', sleep: 349, weight: 89, amt: 210},]
 
 function setToken() {
   if (current_url.includes("token")) {
@@ -33,6 +34,10 @@ function setToken() {
 }
 
 class App extends Component {
+  componentDidMount() {
+
+  }
+
   constructor(props) {
     super(props);
     this.state = setToken();
@@ -58,7 +63,6 @@ class App extends Component {
   }
 
   handleImportClick = () => {
-    console.log(token)
     axios.post(api + '/api/v1/fitbit_data', "", {
       headers: {auth_token: token}
     })
@@ -67,11 +71,21 @@ class App extends Component {
     })
   }
 
+  handleRenderGraph = () => {
+    axios.get(api + '/api/v1/fitbit_data', {
+      headers: {auth_token: token}
+    }).then((response) => {
+      data = response.data
+      console.log(this)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
 
 
 
   render() {
-    console.log(token)
     let button = null;
     let import_button = null;
     let graph = null;
@@ -84,19 +98,19 @@ class App extends Component {
       button = <button onClick={this.handleLogoutClick} type="button">
       Logout
       </button>
-      import_button = <button onClick={this.handleImportClick} type="button">
+      import_button = <button onClick={this.handleRenderGraph} type="button">
       Import Data
       </button>
-      graph =         <LineChart width={900} height={300} data={data}
+      graph = <LineChart width={900} height={300} data={data}
                   margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                <XAxis dataKey="name"/>
+                <XAxis dataKey="date"/>
              <YAxis/>
              <CartesianGrid strokeDasharray="3 3"/>
              <Tooltip/>
              <Legend />
-             <Line type="monotone" dataKey="weight" stroke="#8884d8" activeDot={{r: 8}}/>
-             <Line type="monotone" dataKey="sleep" stroke="#82ca9d" />
+             <Line type="monotone" dataKey="heart_rate" stroke="#8884d8" activeDot={{r: 8}}/>
+             <Line type="monotone" dataKey="rem" stroke="#82ca9d" />
+             <Line type="monotone" dataKey="deep" stroke="#82ca9d" />
               </LineChart>
     }
     return (
