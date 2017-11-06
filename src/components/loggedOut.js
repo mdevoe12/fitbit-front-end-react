b70ae7c976d9e79f8499fe3c596f76c51765ca3b
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-06 04:03:44 -0700

    npm run build finally completed successfully

--- src/components/loggedOut.js
+++ src/components/loggedOut.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ActionButton from './actionButton'
 import axios from 'axios';
-import FitbitApiClient from 'fitbit-node';
+// import FitbitApiClient from 'fitbit-node';
 import '../App.css';
 
 let api = "https://insight-api.herokuapp.com"
@@ -13,10 +13,12 @@ class LoggedOut extends Component {
       headers: {url: window.location.href}
     })
     .then((response) => {
-      let client = new FitbitApiClient(response.data["id"], response.data["secret"])
+      // let client = new FitbitApiClient(response.data["id"], response.data["secret"])
       let scope = 'activity heartrate location nutrition profile settings sleep social weight'
-      let callback = api + '/auth/fitbit/callback'
-      window.location.href = client.getAuthorizeUrl(scope, callback)
+      // let callback = api + '/auth/fitbit/callback'
+      // window.location.href = client.getAuthorizeUrl(scope, callback)
+      window.location.href = "https://www.fitbit.com/oauth2/authorize?response_type=code&" + "client_id=" + response.data["id"] + "&redirect_uri=" + api + "/auth/fitbit/callback&" + "scope=" + scope + "&expires_in=604800"
+
     })
   }
 
